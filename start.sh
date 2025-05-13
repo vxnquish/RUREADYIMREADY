331f335080fc113797f763716fd6c821e87df8c5
Qabin <qabinkicks@gmail.com>
2025-05-13 15:44:57 +0900

    Hi

--- start.sh
+++ start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# 백엔드 실행 (Gradle)
+# 백엔드 실행
 cd backend
 ./gradlew bootRun &
 BACK_PID=$!
 
-# 프론트엔드 실행
+# 종료 시 백엔드도 같이 죽이기 위한 trap
+trap "echo '종료 중...'; kill $BACK_PID" EXIT
+
+# 프론트 실행
 cd ../frontend
 npm run dev
-
-# 프론트 종료 시 백엔드도 종료
-kill $BACK_PID
-
-# 실행 방법
-# New Terminal -> ./chmod +x start.sh (한 번만 하면 됨 다음부턴 그냥 바로)
-# ./start.sh
